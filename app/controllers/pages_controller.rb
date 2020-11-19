9b51edc98486d489daacff67f180561eb4b2be0b
Sixto Guerra <sixto.guerra1982@gmail.com>
2020-11-19 00:52:22 -0300

    buscar j render

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
   def index
-
+    @etiqueta = {dimension_total: "---" , x: 0, y: 1}
   end
 
   def calcular
-    byebug
+    plano = 100
+    @dimensionx =  plano / params[:ancho].to_i
+    @dimensiony = plano / params[:alto].to_i
+    @dimensiontotal = @dimensionx * @dimensiony
+    @etiqueta = {dimension_total: @dimensiontotal , x: @dimensionx, y: @dimensiony}
   end
 end

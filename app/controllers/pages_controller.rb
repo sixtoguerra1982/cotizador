07b57fed5bd7804a7c29c52debad5245937bdc71
Sixto Guerra <sixto.guerra1982@gmail.com>
2020-12-03 00:11:30 -0300

    antes de armar la tabla

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,5 +9,12 @@ class PagesController < ApplicationController
     @dimensiony = plano / params[:alto].to_i
     @dimensiontotal = @dimensionx * @dimensiony
     @etiqueta = {dimension_total: @dimensiontotal , x: @dimensionx, y: @dimensiony}
+    @etiqueta[:dos] = [@dimensiontotal * 2]
+    @etiqueta[:tres] = [@dimensiontotal * 3]
+    @etiqueta[:cuatro] = [@dimensiontotal * 4]
+    @etiqueta[:cinco] = [@dimensiontotal * 5]
+    @etiqueta[:seis] = [@dimensiontotal * 6]
+    @etiqueta[:siete] = [@dimensiontotal * 7]
+    @etiqueta[:ocho] = [@dimensiontotal * 8]
   end
 end
